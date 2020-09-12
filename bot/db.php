e2d5636acf1285bc5c261707f9a01bc7e96dbb29
Artanicus <artanicus@nocturnal.fi>
2020-09-12 11:49:20 +0300

    Set FETCH_ASSOC by default since that's what we predominantly use

--- bot/db.php
+++ bot/db.php
@@ -4,6 +4,7 @@ if($config->DB_HOST && $config->DB_NAME && $config->DB_USER && $config->DB_PASSW
     // Establish PDO connection
     $dbh = new PDO("mysql:host=" . $config->DB_HOST . ";dbname=" . $config->DB_NAME . ";charset=utf8mb4", $config->DB_USER, $config->DB_PASSWORD, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
     $dbh->setAttribute(PDO::ATTR_ORACLE_NULLS, PDO::NULL_EMPTY_STRING);
+    $dbh->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 } else {
     // Write error to log.
     debug_log("Failed to connect to Database!",'!');
