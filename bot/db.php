66c1521caa5678ccda81a2598484d4c391064cc7
Artanicus <artanicus@nocturnal.fi>
2021-07-05 11:52:45 +0300

    Detect missing config or DB errors and fail gracefully
    Also return a 409 error code since this is a "conflict" where the
    request is technically valid and accepted but cannot be completed due to
    a known misconfiguration.

--- bot/db.php
+++ bot/db.php
@@ -6,5 +6,8 @@ if($config->DB_HOST && $config->DB_NAME && $config->DB_USER && $config->DB_PASSW
     $dbh->setAttribute(PDO::ATTR_ORACLE_NULLS, PDO::NULL_EMPTY_STRING);
     $dbh->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 } else {
-    info_log("Failed to connect to Database! Make sure DB_HOST, DB_NAME, DB_USER and DB_PASSWORD are defined!", '!');
+    $error = "Failed to connect to Database! Make sure DB_HOST, DB_NAME, DB_USER and DB_PASSWORD are defined and that you've provided a config/config.json in the first place!";
+    error_log($error);
+    http_response_code(409);
+    die($error);
 }
