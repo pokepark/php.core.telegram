71af01129f5df3d4d3ed2482ba869d8cdcced9dc
Juho-Pekka Kuitunen <artanicus@nocturnal.fi>
2021-07-10 12:44:23 +0300

    Ops excellence (#39)
    * Detect missing config or DB errors and fail gracefully
    
    Also return a 409 error code since this is a "conflict" where the
    request is technically valid and accepted but cannot be completed due to
    a known misconfiguration.
    
    * Exit cleanup requests cleanly with clear errors.
    
    - On no cleanup function, exit with a 404
    - On incorrect CLEANUP_SECRET exit with a 403
    - On an otherwise valid request but with cleanup disabled, edit with a 409

--- bot/cleanup_run.php
+++ bot/cleanup_run.php
@@ -22,20 +22,23 @@ if (isset($update['cleanup']) && $config->CLEANUP) {
             // Run cleanup
             run_cleanup($telegram, $database);
         } else {
-            cleanup_log('No function found to run cleanup!', 'ERROR:');
+            error_log('No function found to run cleanup!');
             cleanup_log('Add a function named "run_cleanup" to run cleanup for telegram messages and database entries!', 'ERROR:');
             cleanup_log('Arguments of that function need to be values to run/not run the cleanup for telegram $telegram and the database $database.', 'ERROR:');
             cleanup_log('For example: function run_cleanup($telegram, $database)', 'ERROR:');
+            http_response_code(404);
         }
     } else {
-        cleanup_log('Error! Wrong cleanup secret supplied!', '!');
+        $error = 'Cleanup authentication failed.';
+        http_response_code(403);
+        error_log($error);
+        die($error);
     }
     // Exit after cleanup
     exit();
 } else if (isset($update['cleanup'])) {
-    cleanup_log('Cleanup request received...');
-    cleanup_log('Cleanup is disabled!');
-    cleanup_log('Please enable it in the config!');
-    cleanup_log('Exiting now...');
-    exit();
+    $error = 'Valid cleanup request received but cleanup is not enabled and will not be done!';
+    error_log($error);
+    http_response_code(409);
+    die($error);
 }
